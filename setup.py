b7b13c4bfeb1bea6eb74079415af86a91fb28119
Martin Bierbaum <martin.bierbaum@rub.de>
2020-08-05 23:36:37 +0200

    black and flake8 code

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='task_runner',
-    description='A task runner to run shell commands on a local computer.',
+    name="task_runner",
+    description="A task runner to run shell commands on a local computer.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author='Martin Bierbaum',
-    license='MIT',
-    py_modules=['task_runner'],
+    author="Martin Bierbaum",
+    license="MIT",
+    py_modules=["task_runner"],
     use_scm_version=True,
-    setup_requires=[
-        'setuptools_scm',
-        ],
-    install_requires=[
-    ],
-    python_requires='>=3.6.2',
+    setup_requires=["setuptools_scm"],
+    install_requires=[],
+    python_requires=">=3.6.2",
 )
--- test.py
+++ test.py
@@ -1,25 +1,33 @@
-
 import os
+import logging
 from task_runner import task, main
 import tempfile
 
+
 @task
 def hello(r):
-    print('Hello world\n')
+    print("Hello world\n")
+
 
 @task
 def pwd(r):
 
     with tempfile.TemporaryDirectory() as tmpdirname:
-        path = os.path.join(tmpdirname, 'white spaces')
+        path = os.path.join(tmpdirname, "white spaces")
         os.mkdir(path)
-        with r.cd(path) as cwd:
-            ret = r.run('pwd')
+        with r.cd(path):
+            r.run("pwd")
+
 
 @task
 def restart(r):
-    r.runas('net stop Apache2.4 & net start Apache2.4')
+    r.runas("net stop Apache2.4 & net start Apache2.4")
 
-if __name__ == '__main__':
+
+@task
+def log(r):
+    logging.info("Hello World!")
+
+
+if __name__ == "__main__":
     main()
-    
